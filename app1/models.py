362f64a859692580172e9e57fee40ae8fc65d89b
shri <shrigavalwad2424@gmail.com>
2023-03-23 22:32:44 +0530

    update in models

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class MyModel(models.Model):
+    name = models.CharField(max_length=250)
+    profession = models.CharField(max_length=250)
